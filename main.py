98824553b51319ca86470d7fccbff3dafd7dae42
Areum An <areum.an.0928@gmail.com>
2025-12-05 11:24:47 -0500

    Add chat UI

--- main.py
+++ main.py
@@ -1,47 +1,51 @@
+import dotenv
+
+dotenv.load_dotenv()
+import asyncio
 import streamlit as st
-import time
+from agents import Agent, Runner, SQLiteSession
+
+if "agent" not in st.session_state:
+    st.session_state["agent"] = Agent(
+        name="ChatGPT Clone",
+        instructions="""
+        You are a helpful assistant.
+        """,
+    )
+agent = st.session_state["agent"]
+
+if "session" not in st.session_state:
+    st.session_state["session"] = SQLiteSession(
+        "chat-history",
+        "chat-gpt-clone-memory.db",
+    )
+session = st.session_state["session"]
 
 
-st.header("Hello world!")
+async def run_agent(message):
+    stream = Runner.run_streamed(
+        agent,
+        message,
+        session=session,
+    )
 
-st.button("Click me please!")
+    async for event in stream.stream_events():
+        if event.type == "raw_response_event":
+            if event.data.type == "response.output_text.delta":
+                with st.chat_message("ai"):
+                    st.write(event.data.delta)
 
-st.text_input(
-    "Write your API KEY",
-    max_chars=20,
-)
 
-st.feedback("faces")
+prompt = st.chat_input("Write a message for your assistant")
+
+if prompt:
+    with st.chat_message("human"):
+        st.write(prompt)
+    asyncio.run(run_agent(prompt))
+
 
 with st.sidebar:
-    st.badge("Badge 1")
-
-
-tab1, tab2, tab3 = st.tabs(["Agent", "Chat", "Output"])
-
-with tab1:
-    st.header("Agent")
-with tab2:
-    st.header("Agent 2")
-with tab3:
-    st.header("Agent 3")
-
-
-with st.chat_message("ai"):
-    st.text("Hello!")
-    with st.status("Agent is using tool") as status:
-        time.sleep(1)
-        status.update(label="Agent is searching the web....")
-        time.sleep(2)
-        status.update(label="Agent is reading the page....")
-        time.sleep(3)
-        status.update(state="complete")
-
-with st.chat_message("human"):
-    st.text("Hi!")
-
-
-st.chat_input(
-    "Write a message for the assistant.",
-    accept_file=True,
-)
+    reset = st.button("Reset memory")
+    if reset:
+        asyncio.run(session.clear_session())
+    st.write(asyncio.run(session.get_items()))
